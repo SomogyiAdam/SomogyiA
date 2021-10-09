93231a8647f0d9159dd452ec62cac70baa24049f
Somogyi Adam <somogyiadam@gmail.com>
2021-10-09 13:20:25 +0200

    szovegdoboz

---
+++ somafood/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>somafood</title>
+</head>
+<body>
+
+    <h1> Ciao!</h1>
+    <form action="index.php" method="POST">
+        <input type="number" name="number1"><br>
+        <input type="text" name="text1"><br>
+        <input type="submit" value="send">
+    </form>
+</body>
+</html>
+
+<?php
+
+if(isset($_POST['number1']))
+{
+    $ho=$_POST['number1'];
+    $szoveg=$_POST['text1'];
+
+if(!empty($ho) || $ho==0)
+{
+if($ho<=0){
+        echo("szilárd $szoveg.");
+}
+else if($ho<100){
+    echo("folyékony. $szoveg.");
+}
+else{
+    echo("légnemű. $szoveg.");
+}
+}
+else{
+    echo("Nem írtál semmit!");
+}  
+
+}
+#$nev="Somogyi Ádám";
+#echo("<h2>Ciao: $nev Have a nice day!:)</h2>");
+?>
